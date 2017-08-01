baaae693e1522ab473c3a295a54b8dc554f642ec
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-08-01 09:12:31 +0200

    Waiting for user input (css)

--- create.sh
+++ create.sh
@@ -2,11 +2,48 @@
 
 echo "Hello! Starting a new project ey?"
 echo "What HTML template engine would you like?"
-echo "
-	1. Standard\n
-	2. Sass\n
-	3. SCSS\n
-	4. Stylus\n
-	5. LESS\n
-	6. PostCSS\n
-"
+
+INVALID_OPTION = "Select valid option"
+
+STANDARD = "1. Standard"
+SASS = "2. Sass"
+SCSS = "3. SCSS"
+STYLUS = "4. Stylus"
+LESS = "5. LESS"
+POSTCSS = "6. PostCSS"
+
+css_choices = (
+  STANDARD
+  SASS
+  SCSS
+  STYLUS
+  LESS
+  POSTCSS
+)
+
+select choice in "${ css_choices[@] }"
+do
+  case $choice in
+    STANDARD)
+      echo STANDRAD
+      ;;
+    SASS)
+      echo SASS
+      ;;
+    SCSS)
+      echo SCSS
+      ;;
+    STYLUS)
+      echo STYLUS
+      ;;
+    LESS)
+      echo LESS
+      ;;
+    POSTCSS)
+      echo POSTCSS
+      ;;
+    *)
+      echo INVALID_OPTION
+      ;;
+  esac
+done
