5a0771bc3c3d4c888a6359f70a320ad29dad9117
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-08-01 08:58:42 +0200

    Init

---
+++ create.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+echo "Hello! Starting a new project ey?"
+echo "What HTML template engine would you like?"
+echo "
+	1. Standard\n
+	2. Sass\n
+	3. SCSS\n
+	4. Stylus\n
+	5. LESS\n
+	6. PostCSS\n
+"
