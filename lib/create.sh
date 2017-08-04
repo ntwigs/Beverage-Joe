d0ec4317c7656f099be25fc8b077aa9ddf746793
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-08-04 09:40:32 +0200

    Create refactor

--- lib/create.sh
+++ lib/create.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 
-path="$(dirname "$0")"
-source ${path}/lib/css.sh
-source ${path}/lib/html.sh
+path="$(dirname "$0")" # Bash directory
+source ${path}/lib/css.sh # get_css_extension get_css_loader_information
+source ${path}/lib/html.sh # get_html_extension get_html_loader_information
 
+function install_main_dependencies() {
+  npm init
+  npm install --save-dev\
+    babel-core\
+    babel-loader\
+    babel-plugin-transform-class-properties\
+    babel-preset-es2015\
+    css-loader\
+    html-loader\
+    html-webpack-plugin\
+    style-loader\
+    url-loader\
+    webpack\
+    webpack-dev-server
+}
 
-
-function create_project() {
-  cd $PWD
-  local html_extension=$(get_html_extension $1)
-  local css_extension=$(get_css_extension $2)
-  
-  # npm init
-  # npm install --save-dev\
-  #   babel-core\
-  #   babel-loader\
-  #   babel-plugin-transform-class-properties\
-  #   babel-preset-es2015\
-  #   css-loader\
-  #   html-loader\
-  #   html-webpack-plugin\
-  #   style-loader\
-  #   url-loader\
-  #   webpack\
-  #   webpack-dev-server
-  local webpack_content=$(get_webpack_content $html_extension $css_extension)
-
+function write_webpack_config() {
   cat << EOF > webpack.config.js 
     $webpack_content
-EOF # This cannot be indented
+EOF # This cannot be indented (End of File)
+}
 
-  # mkdir src
-  # cd src
-  # mkdir image
-  # mkdir js
-  # mkdir style
-  # mkdir view
-  # touch view/index.$html_extension
-  # touch style/index.$css_extension
-  # echo "import '../style/index.$css_extension'" >> js/index.js
+function create_folder_structure() {
+  mkdir src
+  cd src
+  mkdir image
+  mkdir js
+  mkdir style
+  mkdir view
+  touch view/index.$1
+  touch style/index.$2
+  echo "import '../style/index.$2'" >> js/index.js
 }
 
 function get_webpack_content() {
@@ -84,14 +81,8 @@ function get_webpack_content() {
               }
             }],
           },
-          {
-            test: /\.$2$/,
-            loaders: 'style-loader!css-loader?minimize=true!stylus-loader'
-          },
-          {
-            test: /\.pug$/,
-            loader: 'html-loader!pug-html-loader?pretty&exports=false'
-          },
+          $4
+          $5
           {
             test: /\.(png|jpg|gif)$/,
             loaders: 'url-loader'
@@ -100,4 +91,19 @@ function get_webpack_content() {
       },
     }
   "
+}
+
+function create_project() {
+  cd $PWD
+
+  install_main_dependencies
+  
+  local html_extension=$(get_html_extension $1)
+  local css_extension=$(get_css_extension $2)
+  local html_loader=$(get_html_loader_information $1)
+  local css_loader=$(get_css_loader_information $2)
+  local webpack_content=$(get_webpack_content $html_extension $css_extension $html_loader $css_loader)
+
+  write_webpack_config
+  create_folder_structure $html_extension $css_extension
 }
