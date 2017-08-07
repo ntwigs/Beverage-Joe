611227e56b4b4435b191c91a9c674cf86160dc76
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-08-07 22:19:04 +0200

    Added config file

---
+++ config.sh
@@ -0,0 +1,15 @@
+## Are you unhappy with the architecture? Look no further! ##
+
+## MAIN FOLDER ##
+root_name="static" # Default: static
+
+## SUB FOLDERS ##
+images_folder="images" # Default: images
+stylesheet_folder="style" # Default: style
+pages_folder="views" # Default: views
+javascript_folder="js" # Default: js
+
+## INDEX FILES ##
+stylesheet_index="index" # Default: index
+html_index="index" # Default: index
+javascript_index="index" # Default: index
