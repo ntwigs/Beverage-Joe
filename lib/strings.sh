30d9e7b56b698ee39062a7f9e47a799bf7f871e6
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-08-07 23:33:35 +0200

    string formatting

--- lib/strings.sh
+++ lib/strings.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ${path}/config.sh # get_package get_webpack_content
+
 # ENTRY MESSAGES #
 confirmation="Are you ok with this? (y/n/q) "
 no_name="
@@ -21,122 +23,108 @@ invalid_option="Select valid option"
 # HTML #
 
 ## HTML MESSAGES ##
-html="What HTML template engine would you like?: "
+html_message="What HTML template engine would you like?: "
 
 ## HTML LOADERS ##
-html_loader="
-  {
-    test: /\.html$/,
-    loader: 'html-loader'
-  },
-"
+html_loader="{
+            test: /\.html$/,
+            loader: 'html-loader'
+          },"
 
-haml_loader="
-  {
-    test: /\.(html|haml|hamlc)$/,
-    loader: 'haml-loader'
-  },
-"
+haml_loader="{
+            test: /\.(html|haml|hamlc)$/,
+            loader: 'haml-loader'
+          },"
 
-pug_loader="
-  {
-    test: /\.pug$/,
-    loader: 'html-loader!pug-html-loader?pretty&exports=false'
-  },
-"
+pug_loader="{
+            test: /\.pug$/,
+            loader: 'html-loader!pug-html-loader?pretty&exports=false'
+          },"
 
-handlebars_loader="
-  {
-    test: /\.(hbs|handlebars)$/,
-    loader: 'handlebars-loader'
-  },
-"
+handlebars_loader="{
+            test: /\.(hbs|handlebars)$/,
+            loader: 'handlebars-loader'
+          },"
 
 # CSS #
 
 ## CSS MESSAGES ##
-css="What CSS post-processor would you like?: "
+css_message="What CSS post-processor would you like?: "
 
 ## CSS LOADERS ##
-css_loader="
-  {
-    test: /\.css$/,
-    loaders: 'style-loader!css-loader?minimize=true'
-  },
-"
-sass_loader="
-  {
-    test: /\.scss$/,
-    loaders: 'style-loader!css-loader?minimize=true!sass-loader'
-  },
-"
-stylus_loader="
-  {
-    test: /\.styl$/,
-    loaders: 'style-loader!css-loader?minimize=true!stylus-loader'
-  },
-"
-less_loader="
-  {
-    test: /\.less$/,
-    loaders: 'style-loader!css-loader?minimize=true!less-loader'
-  },
-"
+css_loader="{
+            test: /\.css$/,
+            loaders: 'style-loader!css-loader?minimize=true'
+          },"
+          
+sass_loader="{
+            test: /\.scss$/,
+            loaders: 'style-loader!css-loader?minimize=true!sass-loader'
+          },"
+
+stylus_loader="{
+            test: /\.styl$/,
+            loaders: 'style-loader!css-loader?minimize=true!stylus-loader'
+          },"
+
+less_loader="{
+            test: /\.less$/,
+            loaders: 'style-loader!css-loader?minimize=true!less-loader'
+          },"
 
 ## GENERATE WEBPACK CONTENT ##
 function get_webpack_content() {
   echo "
-    const path = require('path')
-    const webpack = require('webpack')
-    const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-    module.exports = {
-      context: path.resolve(__dirname, './src'),
-      entry: {
-        index: './view/index.$1',
-        bundle: './js/index.js'
-      },
-      output: {
-        path: path.resolve(__dirname, './build'),
-        filename: '[name].bundle.js',
-        publicPath: '/'
-      },
-      devServer: {
-        contentBase: path.resolve(__dirname, './src'),
-        watchContentBase: true,
-        hot: true,
-        inline: true,
-      },
-      plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-          title: 'index.html',
-          template: 'view/index.$1'
-        })
-      ],
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: [/node_modules/],
-            use: [{
-              loader: 'babel-loader',
-              options: { 
-                presets: ['es2015'],
-                plugins: ['transform-class-properties']
-              }
-            }],
-          },
-          $2
-          $3
-          {
-            test: /\.(png|jpg|gif)$/,
-            loaders: 'url-loader'
+module.exports = {
+  context: path.resolve(__dirname, './$root_folder'), // Dev Source
+  entry: {
+    index: './$pages_folder/$pages_index.$1', // Index HTML file
+    bundle: './$javascript_folder/$javascript_index.js' // Index JavaScript file
+  },
+  output: {
+    path: path.resolve(__dirname, './$build_folder'), // Build folder
+    filename: '[name].bundle.js',
+    publicPath: '/' // The Build folder root
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './$root_folder'), // Dev Source
+    watchContentBase: true,
+    hot: true,
+    inline: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'index.html',
+      template: '$pages_folder/$pages_index.$1' // Index HTML file
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { 
+            presets: ['es2015'],
+            plugins: ['transform-class-properties']
           }
-        ],
+        }],
       },
-    }
-  "
+      $2
+      $3
+      {
+        test: /\.(png|jpg|gif)$/,
+        loaders: 'url-loader'
+      }
+    ],
+  },
+}"
 }
 
 ## GENERATE PACKAGE.JSON CONTENT ##
